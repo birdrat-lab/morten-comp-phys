332137b839372304566feb24e8ae1dc17a84ed25
birdrat <evelyn.demastri@protonmail.com>
2024-07-12 02:39:29 -0400

    finished first exercise (need to add comments)

--- ch2/decimal_to_binary/2_6_1.cpp
+++ ch2/decimal_to_binary/2_6_1.cpp
@@ -1,93 +1,66 @@
-#include<functional> 
 #include <iostream>
 #include <cmath>
 
-const int MAX_POWER=1000;
-const int MIN_POWER=-1000;
-
-int get_sign(double);
-int leading_power(double);
-// int scale(double, std::function<double(double)>, std::function<bool(double)>, bool*);
 
 int
 main
-()
+(int argc, char *argv[])
 {
 
-  double decimal;
-
+  double decimal, approx;
   int sign, precision, first_order;
-
-  std::cout << "Enter decimal to convert: " << std::endl;
-  std::cin >> decimal;
-  
-  std::cout << "To what precision should the conversion be carried: " << std::endl;
-  std::cin >> precision;
-
-  sign = get_sign(decimal);
-  if(!sign){std::cout<< "Binary representation: " <<  0 << std::endl; return 0;}
-
-  decimal = std::abs(decimal);
-  
-  std::cout << "leading power: " << sign <<"*2^"<<leading_power(decimal)<<std::endl;
-  
-  int binary[precision];
-  
-  return 0;
-
-}
-
-int
-get_sign
-(double decimal)
-{
-  return ((decimal>0)-(decimal<0));
-}
-
-int
-leading_power
-(double decimal)
-{
-  int lower_bound, upper_bound;
-  int guess;
-  bool exact;
-  
-  std::function<bool(double,int,bool*)> decimal_is_higher=[&](double x,int exp, bool *is_exact)
-  {
-    double power = std::pow(2,exp);
-    if(std::abs(power-decimal)<=1e-16){*is_exact=true;}
-    return (x>power);
-  };
-
-  lower_bound=MIN_POWER;
-  upper_bound=MAX_POWER;
-  guess=0;
-  exact=false;
-    
-  //  while
-  //  (std::abs(lower_bound-upper_bound<=1))
-  int i = 0;
-  while
-    (i<6);
+   
+  if
+    (argc==2)
     {
+      std::cout << "No precision specified, assuming default precision of 16 bits." << std::endl;
+      decimal = std::atof(argv[1]);
+      precision=16;
+    }
+  else if
+    (argc==3)
+    {
+      decimal = std::atof(argv[1]);
+      precision = std::atoi(argv[2]);
+    }
+  else
+    {
+      std::cout << "Please input two arguments: a decimal in scientific notation," <<
+         "followed by the number of bits of percision desired in the conversion to binary." << std::endl;
+      return 1;
+    
+    }
+  
+  first_order = (int) std::floor(std::log2(decimal));
+
+  approx = std::pow(2,first_order);
+  
+
+  std::cout << "1.";
+  
+  for
+    (int j = first_order-1; j>=first_order-precision; j--)
+    {
+
+      double increase = std::pow(2,j); 
+
+      
+      
       if
-	(decimal_is_higher(decimal, guess, &exact))
+	((increase+approx)<=decimal)
 	{
-	  lower_bound=guess;
+	  approx += increase;
+	  std::cout << "1";
 	}
       else
 	{
-	  upper_bound=guess;
-	}
+	  std::cout << "0";
+	};
+     
+    };
 
-      if(exact){return guess;};
+  std::cout << "e"<<first_order << std::endl;
 
-      std::cout<<guess<<std::endl;
-      
-      guess=floor(upper_bound-lower_bound)*0.5;
-      i++;
-    }
-
-  return guess;
-
+  return 0;
+  
 }
